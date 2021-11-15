bded958f622ea07d00c5f9f16eb9d44b6115ca2d
nhavilah <51215537+nhavilah@users.noreply.github.com>
2021-11-15 11:23:52 +1000

    Removing unnecessary stuff

--- src/pages/UpdateShifts.js
+++ src/pages/UpdateShifts.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useState, useEffect} from "react";
+import {useState} from "react";
 import {useHistory} from "react-router-dom";
 
 function UpdateShifts() {
@@ -7,43 +7,6 @@ function UpdateShifts() {
     const params = new URLSearchParams(window.location.search);
     let id = params.get("id")
     const [formData, setFormData] = useState({start: "2018-01-01 10:15", finish: "2018-01-01 12:20", breakLength: 50})
-    const [selection, setSelection] = useState("")
-    // get users
-    const [users, setUsers] = useState([])
-    useEffect(() => {
-        fetch('http://127.0.0.1:3000/users', {
-            method: "get",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": sessionStorage.getItem("sessionId")
-            }
-        }).then(response => response.json()).then(data => {
-            setUsers(data)
-        })
-    }, [])
-    // get shifts
-    const [shifts, setShifts] = useState([])
-    useEffect(() => {
-        fetch('http://127.0.0.1:3000/shifts', {
-            method: "get",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": sessionStorage.getItem("sessionId")
-            }
-        }).then(response => response.json()).then(data => {
-            setShifts(data)
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].id == id) {
-                    setFormData({
-                        ...formData,
-                        start: data[i].start,
-                        finish: data[i].finish,
-                        breakLength: data[i].breakLength
-                    })
-                }
-            }
-        })
-    }, [])
 
     const authenticate = (e) => {
         e.preventDefault()
